4d1f254cd7ccda3d96684beb314109f61006547a
Fetter <nfett296@uwsp.edu>
2017-11-14 22:21:37 -0600

    Recovered playlist... .sql

--- playlistByStartAndEndTime.sql
+++ playlistByStartAndEndTime.sql
@@ -1,17 +1,3 @@
-/*delimiter //
-Drop procedure if exists `playlist`//
-Create Procedure playlist
-	(In startTime datetime,
-    In endTime datetime)
-    Begin
-    
-    Select * 
-    from played;
-
-	End//
-delimiter ;
-    Call playlist(now(), now());    
-  */  
     
 Set @startTime = now();
 Set @endTime = now();
