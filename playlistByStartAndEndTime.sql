1d6f05a65240356faa685f6bb3f5c29c6da4105c
Nathan Fetter <nfett296@uwsp.edu>
2017-11-15 10:00:30 -0600

    ye

--- playlistByStartAndEndTime.sql
+++ playlistByStartAndEndTime.sql
@@ -16,5 +16,9 @@ delimiter ;
 Set @startTime = now();
 Set @endTime = now();
     
-    Select * 
-    from played;
+    Select stack.name, song.title, artist.name,
+		albumn.name, label.name
+    from played, stack, song, artist, albumn, label
+    where played.stackID = stack.id AND played.songID = song.id AND song.albumnID = albumn.id AND 
+		albumn.labelID = label.id AND song.artistID = artist.id AND played.time between startTime and endTime
+	ORDER BY played.time asc;
